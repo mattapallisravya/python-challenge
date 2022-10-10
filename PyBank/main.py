c33f4a43a9bd359ef0782f5d7e987313c30cba13
4109 <mattapallisravya@gmail.com>
2022-10-10 18:24:54 -0500

    Updated code for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,12 +5,8 @@ import csv
 csvpath = os.path.join('..','PyBank', 'Resources', 'budget_data.csv')
 print("Financial Analysis")
 print("-----------------------------------------------")
-numberOfMonths=[]
-amount=[]
-changeInProfit=[]
 total=0
-#greatestIncreaseInProfits=0
-#greatestDecreaseInProfits=0
+
 
 
 
@@ -20,34 +16,40 @@ with open(csvpath) as budgetFile:
     csvreader = csv.reader(budgetFile, delimiter=',')
 
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
-    counter=0
-    for items in csvreader:
-        total=total+int(items[1])
-        counter=counter+1
-        amount.append(int(items[1]))
-        numberOfMonths.append(items[0])
-    print("Total Months : ", len(numberOfMonths))
     
-    for num in range(0,len(amount)-1):
-        #total=total+amount[num]
-        change=amount[num+1]-amount[num]
-        changeInProfit.append(change)
-    averageChange=sum(changeInProfit)/len(changeInProfit)
-    greatestIncreaseInProfits=max(changeInProfit)
-    greatestDecreaseInProfits=min(changeInProfit)
+    counter=0
+    greatestIncreaseInProfit=0
+    greatestDecreaseInProfit=0
+    sumOfChangeInProfit=0
+    firstRow=True
+    for items in csvreader:
+        if firstRow==True:
+            total=total+int(items[1])
+            previousRow=int(items[1])
+            counter=counter+1
+            firstRow=False
+        else:
+            total=total+int(items[1])
+            netChange=int(items[1])-previousRow
+            previousRow=int(items[1])
+            sumOfChangeInProfit += netChange
+            counter=counter+1
+            if netChange > greatestIncreaseInProfit:
+                greatestIncreaseInProfit = netChange
+                greatestIncreaseInProfitmonth = items[0]
+            elif netChange<greatestDecreaseInProfit:
+                greatestDecreaseInProfit=netChange
+                greatestDecreaseInProfitmonth=items[0]
+        
+        
+       
+    
+    averageChange=sumOfChangeInProfit/counter-1
+   
 
     print("Total : ",total)
     print(f"Average Change : ${round(averageChange,2)}")
-    print(f"Greatest Increase In Profits : {numberOfMonths[changeInProfit.index(greatestIncreaseInProfits)+1]} (${greatestIncreaseInProfits})")
-    print(f"Greatest Decrease In Profits : {numberOfMonths[changeInProfit.index(greatestDecreaseInProfits)+1]} (${greatestDecreaseInProfits})")
-    #print(len(changeInProfit))
-    #print(len(numberOfMonths))
-            
-       
-        
-
+    print(f'Greatest Increase In Profits : {greatestIncreaseInProfitmonth} (${greatestIncreaseInProfit})')
+    print(f'Greatest Increase In Profits : {greatestDecreaseInProfitmonth} (${greatestDecreaseInProfit})')
     
-
-
-
+    
